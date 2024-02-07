0be35ea1223a9e9c69e29e02dd143e896f29e488
AleksanderSun <asuntcev@gmail.com>
2024-02-07 19:49:54 +0300

    Второй вариант. Внес изменения в соответствии с замечаниями.

--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -1,19 +1,19 @@
 public class Formatter {
-    String rightTotal;
-    public void rightFormat(double floatTotal) {
+    public String rightFormat(double floatTotal) {
         double total = (Math.floor(floatTotal)) % 100;
         double t = total % 10;
                 if (total == 0) {
-                    rightTotal = String.format("%.2f", floatTotal) + " рублей";
+                   String rightTotal = String.format("%.2f", floatTotal) + " рублей";
                 } else if (total > 10 & total <20) {
-                    rightTotal = String.format("%.2f", floatTotal) + " рублей";
+                    return String.format("%.2f", floatTotal) + " рублей";
                 } else if (t == 1) {
-                    rightTotal = String.format("%.2f", floatTotal) + " рубль";
+                    return String.format("%.2f", floatTotal) + " рубль";
                 } else if (t > 1 & t < 5) {
-                    rightTotal = String.format("%.2f", floatTotal) + " рубля";
+                    return String.format("%.2f", floatTotal) + " рубля";
                 } else {
-                    rightTotal = String.format("%.2f", floatTotal) + " рублей";
+                    return String.format("%.2f", floatTotal) + " рублей";
                 }
 
+        return null;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        scanner.useDelimiter("\n");
-        int guests;
-        System.out.println("Здраствуйте!\nНа сколько человек необходимо разделить счет?");
-        while (true) {
-            if (scanner.hasNextInt()) {
-                guests = scanner.nextInt();
-
-                if (guests == 1) {
-                    System.out.println("Вы один. В этом случае нет смысла ничего считать и делить.");
-                } else if (guests <= 1) {
-                    System.out.println("Это некорректное значение для подсчёта.");
-                } else if (guests >= 1) {
-                    System.out.println("Включаем калькулятор товаров!");
-                    break;
-                }
-            } else {
-                System.out.println("Это неправильное число!");
-                scanner.next();
-            }
-        }
-        Calculator calculator = new Calculator();
-        calculator.sumProduct();
-        System.out.println(calculator.allProduct);
-        double totalCost = calculator.sum / guests;
-        Formatter formatter = new Formatter();
-        formatter.rightFormat(totalCost);
-        System.out.println("Счет поделен на: " + guests + " гостей. " + "С каждого по " + formatter.rightTotal + ".");
+         Scaner scan = new Scaner();
+        scan.scaner();
     }
 }
